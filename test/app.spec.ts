840d940434e9a03e5abcff94c9967c1b773121ea
barbapapazes <e.soubiran25@gmail.com>
2024-08-09 11:10:02 +0200

    test: add initial tests

---
+++ test/app.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+import { setup, createPage } from "@nuxt/test-utils/e2e";
+
+describe("App", async () => {
+  await setup({
+    host: process.env.CI ?? "http://localhost:3000",
+  });
+
+  test("contains the title 'Welcome to Nuxt!'", async () => {
+    const page = await createPage("/");
+
+    expect(await page.innerHTML("h1")).toBe("Welcome to Nuxt!");
+  });
+});
